0995d3cfee5255a48335fbce0a0ae640abe465d9
Irving Ruan <irvingruan@gmail.com>
2012-03-28 17:55:45 -0700

    Problem 14 solution to Collatz problem, optimized with recurrence

--- Problem14.py
+++ Problem14.py
@@ -16,3 +16,41 @@
 
 import math
 
+def sequence(n):
+    longest_counter = 0   
+    num = 3
+    save = 0
+    
+    recurrence_dict = {}
+    
+    while num < n:
+        
+        counter = 0
+        check = num
+        key = num
+        
+        while check != 1:
+            if check % 2 == 0:
+                check = check / 2
+            else:
+                check = (check * 3) + 1
+                
+            counter += 1        
+            recurrence_dict[key] = counter
+            
+            if check in recurrence_dict:
+                recurrence_dict[key] = recurrence_dict[check] + counter
+                counter = recurrence_dict[key]
+                break
+         
+            
+        if counter >= longest_counter:
+            longest_counter = counter
+            save = num
+        
+        num += 1
+        
+    print save, longest_counter
+    
+sequence(1000000)
+
